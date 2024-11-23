e0a4d41231831136daded0776d105a673e402e5f
Stephen1303 <wndystvn13@mhs.mdp.ac.id>
2024-11-23 12:18:52 +0700

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+
+function Counter() {
+  const [count, setCount] =useState(0);
+
+  useEffect(() => {
+    document.title = `${count} times clicked`
+  }, [count]);
+
+  return (
+      <div>
+          <button onClick={() => setCount(count+1)}>
+              Change Counter {count} times clicked
+          </button>
+      </div>
+  );
+}
+
+const UserCard = (props) => {
+  return (
+    <div>
+      <h1>My Identity</h1>
+      <ul>
+        <li>Nama : {props.nama}</li>
+        <li>NPM : {props.npm}</li>
+        <li>
+          My Github :
+          <a href="">{props.github}</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Form(){
+  const [name, setName] = useState('');
+
+  const handleSubmit = () => {
+    if (name.trim()) {
+      alert(`Hallo, ${name}`);
+    } else {
+      alert('Harap Masukkan nama!');
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Masukkan Nama Anda"/>
+      <button type="submit">Tampilkan</button>
+    </form>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div >
+        <Counter />
+        <UserCard nama="Wendy Steven" npm="2428240056P" github="My_Github"/>
+        <Form />
+      </div>
     </div>
   );
 }
